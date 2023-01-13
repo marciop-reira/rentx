66f3559f61e23f9d86c13f2b947eec934394d230
Márcio Pereira <marciop.usa@gmail.com>
2023-01-12 21:49:04 -0300

    Categories routes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { CategoryRepository } from "../repositories/CategoryRepository";
+
+const categoriesRoutes = Router();
+const categoryRepository = new CategoryRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  categoryRepository.create({ name, description });
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
