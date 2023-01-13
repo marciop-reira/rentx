d0a9eaa2d4e1b112e049f7c623ff5cc19995f434
Márcio Pereira <marciop.usa@gmail.com>
2023-01-12 22:03:53 -0300

    list route and category already exists validation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,22 @@ const categoryRepository = new CategoryRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return response.status(400).json({
+      message: "Category already exists.",
+    });
+  }
+
   categoryRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const categories = categoryRepository.list();
+
+  return response.json({ categories });
 });
 
 export { categoriesRoutes };
